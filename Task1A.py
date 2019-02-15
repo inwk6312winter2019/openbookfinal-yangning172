59160ea5ca810e9f72a5716396ca95f8f910cb21
yangning172 <nn531884@dal.ca>
2019-02-15 15:10:03 -0400

    modified

--- Task1A.py
+++ Task1A.py
@@ -1,12 +1,10 @@
-def unique_words(file):
-    mylist = []
-    
-    f = open(file)
-    import string
-    for line in f:
-       pass
-
-
+def unique_words(book):
+	unique_list=[]
+	for line in book:
+		for word in line.split():
+			if word not in unique_list:
+				unique_list.append(word)
+	print (unique_list)
 
 def count_the_article(file):
     f = open(file)
@@ -23,5 +21,47 @@ def count_the_article(file):
         for punc in string.punctuation:
             word = word.replace(punc, ' ')
         count += len(word.split(' '))
-    print(count) 
+    print(count)
+
+
+
+
+def sorted_words(book):
+	sorted_list=[]
+        for line in book:
+                for word in line.split():
+                        if word not in sorted_list:
+                                sorted_list.append(word)
+				sorted(sorted_list)
+	print (sorted_list)
+
+def character_word_count(book):
+	my_dict={}
+	list1=[]
+	book_copy=book
+	for line in book:
+                for word in line.split():
+                        if word not in list1:
+				list1.append(word)
+	for list_words in list1:
+		count=0
+		for lines in book_copy:
+                	for word1 in lines.split():
+				if word1==list_words:
+					count+=1
+		my_dict[list_words]=count
+	print (my_dict)
+
+def starts_with_vow(book):
+	count =0
+        for line in book:
+                for word in line.split():
+                        if word[0] in ("a","e","i","o","u"):
+				count=count+1
+	return (count)
+
+
+
+print(starts_with_vow(book_open))
+
 
