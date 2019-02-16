ca9fb43a29f18da0934daa943c148b01ce45841a
yangning172 <nn531884@dal.ca>
2019-02-16 09:25:56 -0400

    final version

--- Task1A.py
+++ Task1A.py
@@ -1,4 +1,4 @@
-#Function for unique_words
+# Task1A_1
 def unique_words(book):
     f = open(book)
     mylist1 = []
@@ -23,9 +23,9 @@ def unique_words(book):
     return mylist2
 
 
-unique_words('Book1.txt')
+# unique_words('Book1.txt')
 
-
+# Task1A_2
 def count_the_article(file):
     f = open(file)
     mylist = list()
@@ -44,43 +44,68 @@ def count_the_article(file):
     print(count)
 
 
-
-
+# Task1A_3
 def sorted_words(book):
-	sorted_list=[]
-        for line in book:
-                for word in line.split():
-                        if word not in sorted_list:
-                                sorted_list.append(word)
-				sorted(sorted_list)
-	print (sorted_list)
+    f = open(book)
+    mylist = []
+    d = dict()
+    for line in f:
+        line = line.strip().lower()
+        import string
+        for punc in string.punctuation:
+            line = line.replace(punc,' ')
+        for word in line.split():
+            mylist.append(word)
+    for key in mylist:
+        if key not in d:
+            d[key]=1
+        else:
+            d[key]+=1
+    list2 = sorted(d)
+    print(list2[::-1])
 
+# sorted_words('Book1.txt')
+
+
+# Task1A_4
 def character_word_count(book):
-	my_dict={}
-	list1=[]
-	book_copy=book
-	for line in book:
-                for word in line.split():
-                        if word not in list1:
-				list1.append(word)
-	for list_words in list1:
-		count=0
-		for lines in book_copy:
-                	for word1 in lines.split():
-				if word1==list_words:
-					count+=1
-		my_dict[list_words]=count
-	print (my_dict)
-
-def starts_with_vow(book):
-	count =0
-        for line in book:
-                for word in line.split():
-                        if word[0] in ("a","e","i","o","u"):
-				count=count+1
-	return (count)
+    f = open(book)
+    d = dict()
+    for line in f:
+        line = line.strip().lower()
+        import string
+        for punc in string.punctuation:
+            line = line.replace(punc,' ')
+        for word in line.split():
+            if word not in d:
+                d[word] = len(word)
+    print(d)
+    return d
 
 
 
-print(starts_with_vow(book_open))
+# Task1A_5
+def start_with_vow(book):
+    f = open(book)
+    list1 = ['a','e','i','o','u']   #all is lower letter means that I need transfer words in the book in lower letter to match
+    list2 = []
+    list3 = []
+    count = 0
+    for line in f:
+        line = line.strip().lower()
+        import string
+        for punc in string.punctuation:
+            line = line.replace(punc,' ')
+        for word in line.split():
+            list2.append(word)
+    for k in list2:
+        if k[0] in list1:
+            count += 1
+            list3.append(k)
+    print(list3)         # check the list  whether just includ words start from 'a','e','i','o','u'
+    print(count)
+    return count
+
+
+start_with_vow('Book1.txt')
 
